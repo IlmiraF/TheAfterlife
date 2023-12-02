0543c8ef1f4a2ebe97e0395a526c4609db2893e2
VlasovEgor <vlasovegor2@gmail.com>
2023-12-02 20:05:16 +0300

    add Health system and Death
    Added CharacterAttributeComponent and Healthattribute
    Added Deathevent and OnDeathanimations

---
+++ Source/TheAfterlife/HomeTheAfterlife/Animations/Notifies/AnimNotify_EnableRagdoll.cpp
@@ -0,0 +1,8 @@
+
+#include "AnimNotify_EnableRagdoll.h"
+
+void UAnimNotify_EnableRagdoll::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	MeshComp->SetCollisionProfileName(FName("Ragdoll"));
+	MeshComp->SetSimulatePhysics(true);
+}
--- Source/TheAfterlife/HomeTheAfterlife/Core/Characters/PlayerCharacter.h
+++ Source/TheAfterlife/HomeTheAfterlife/Core/Characters/PlayerCharacter.h
@@ -17,6 +17,8 @@ class THEAFTERLIFE_API APlayerCharacter : public ABaseCharacter
 public:
 	APlayerCharacter(const FObjectInitializer& ObjectInitializer);
 
+	virtual void BeginPlay() override;
+
 	virtual void MoveForward(float value) override;
 	virtual void MoveRight(float value) override;
 	virtual void Turn(float value) override;
@@ -25,8 +27,6 @@ public:
 	virtual void Landed(const FHitResult& Hit) override;
 
 protected:
-
-	virtual void BeginPlay() override;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character|Camera")
 	class UCameraComponent* CameraComponent;
@@ -37,7 +37,16 @@ protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character|Jump")
 	class UAnimMontage* DoubleJumpMontage;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character|Components")
+	class UCharacterAttributeComponent* CharacterAttributeComponent;
+
+	virtual void OnDeath();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character | Animations")
+	class UAnimMontage* OnDeathAnimMontage;
+
 private:
 	int32 JumpCount = 0;
 	
+	void EnableRagdoll();
 };
