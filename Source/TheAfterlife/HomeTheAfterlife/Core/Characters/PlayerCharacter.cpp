c800e4d766d22b3e55d69dc7208436354a0c4093
VlasovEgor <vlasovegor2@gmail.com>
2023-12-03 13:59:40 +0300

    Revert "added the damage of falling from a height"
    This reverts commit 9d5b1804eea16a12c85ede1cf47fd0512318c9ad.

--- Source/TheAfterlife/HomeTheAfterlife/Core/Characters/PlayerCharacter.cpp
+++ Source/TheAfterlife/HomeTheAfterlife/Core/Characters/PlayerCharacter.cpp
@@ -6,7 +6,6 @@
 #include "Camera/CameraComponent.h"
 #include "../Components/MovementComponents/BaseCharacterMovementComponent.h"
 #include "../Components\CharacterComponents\CharacterAttributeComponent.h"
-#include "Engine/DamageEvents.h"
 
 APlayerCharacter::APlayerCharacter(const FObjectInitializer& ObjectInitializer)
 	:Super(ObjectInitializer)
@@ -82,25 +81,6 @@ void APlayerCharacter::Landed(const FHitResult& Hit)
 {
 	Super::Landed(Hit);
 	JumpCount = 0;
-
-	float FallHeight = (CurrentFallApex - GetActorLocation()).Z * 0.01;
-
-	if (IsValid(FallDamageCurve) == true)
-	{
-		float DamageAmount = FallDamageCurve->GetFloatValue(FallHeight);
-		TakeDamage(DamageAmount, FDamageEvent(), GetController(), Hit.GetActor());
-	}
-}
-
-void APlayerCharacter::NotifyJumpApex()
-{
-	Super::NotifyJumpApex();
-	CurrentFallApex = GetActorLocation();
-}
-
-void APlayerCharacter::Falling()
-{
-	GetBaseCharacterMovementComponent()->bNotifyApex = true;
 }
 
 void APlayerCharacter::BeginPlay()
