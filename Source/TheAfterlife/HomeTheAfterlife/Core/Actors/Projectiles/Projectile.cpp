46b7125dc94d16ccb3a92521bd8337e2c4dca25c
VlasovEgor <vlasovegor2@gmail.com>
2024-01-07 22:08:27 +0300

    New projectile class, projectile bomb blueprint

---
+++ Source/TheAfterlife/HomeTheAfterlife/Core/Actors/Projectiles/Projectile.cpp
@@ -0,0 +1,23 @@
+#include "Projectile.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+AProjectile::AProjectile()
+{
+    CollisionComponent = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionComponent"));
+    CollisionComponent->InitSphereRadius(5.0f);
+    CollisionComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    CollisionComponent->SetCollisionResponseToAllChannels(ECR_Block);
+    SetRootComponent(CollisionComponent);
+
+    ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("MovementComponent"));
+    ProjectileMovementComponent->InitialSpeed = 2000.f;
+
+}
+
+void AProjectile::LaunchProjectile(FVector Diretion)
+{
+    ProjectileMovementComponent->Velocity = Diretion * ProjectileMovementComponent->InitialSpeed;
+    CollisionComponent->IgnoreActorWhenMoving(GetOwner(), true);
+
+}
---
+++ Source/TheAfterlife/HomeTheAfterlife/Core/Actors/Projectiles/Projectile.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Projectile.generated.h"
+
+UCLASS()
+class THEAFTERLIFE_API AProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+
+	AProjectile();
+
+	UFUNCTION(BlueprintCallable)
+	void LaunchProjectile(FVector Diretion);
+
+protected:
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	class USphereComponent* CollisionComponent;
+
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	class UProjectileMovementComponent* ProjectileMovementComponent;
+
+
+};
