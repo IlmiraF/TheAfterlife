1187f5b378f7de6b16fa49cb9cb92a8b9e6f56b2
VlasovEgor <vlasovegor2@gmail.com>
2024-03-25 20:27:54 +0300

    added the ability for the conductor to speak

---
+++ Source/TheAfterlife/HomeTheAfterlife/Core/Characters/Conductor/Conductor.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Components/AudioComponent.h"
+#include "Conductor.h"
+
+AConductor::AConductor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	CurrentSpeechIndex = -1;
+
+	ConductorMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMesh"));
+	ConductorMesh->SetupAttachment(GetRootComponent());
+
+	ConductorAudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("AudioComponent"));
+	ConductorAudioComponent->SetupAttachment(ConductorMesh);
+}
+
+void AConductor::StartSpeaking()
+{
+	ConductorAudioComponent->SetSound(WealcomSpeech);
+	ConductorAudioComponent->Play();
+
+	GetWorld()->GetTimerManager().SetTimer(SpeechTimerHandle, this, &AConductor::Speak, WaitingTimeBetweenGreetingAndDiscontent, false);
+}
+
+
+void AConductor::Speak()
+{	
+	CurrentSpeechIndex++;
+	if (CurrentSpeechIndex < WaitingSpeeches.Num())
+	{	
+		ConductorAudioComponent->SetSound(WaitingSpeeches[CurrentSpeechIndex]);
+		ConductorAudioComponent->Play();
+		GetWorld()->GetTimerManager().SetTimer(SpeechTimerHandle, this, &AConductor::Speak, WaitingTimeBetweenPhrasesDiscontent, false);
+	}
+}
---
+++ Source/TheAfterlife/HomeTheAfterlife/Core/Characters/Conductor/Conductor.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "..\..\Actors\Interfaces\ISpeak.h"
+#include "Conductor.generated.h"
+
+UCLASS()
+class THEAFTERLIFE_API AConductor : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	AConductor();
+
+	void StartSpeaking();
+
+private:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	class USkeletalMeshComponent* ConductorMesh;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	UAudioComponent* ConductorAudioComponent;
+
+	UPROPERTY(EditAnywhere, Category = "Speach")
+	USoundBase* WealcomSpeech;
+
+	UPROPERTY(EditAnywhere, Category = "Speach")
+	TArray<USoundBase*> WaitingSpeeches;
+
+	UPROPERTY(EditAnywhere, Category = "Speach")
+	float WaitingTimeBetweenGreetingAndDiscontent = 120.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Speach")
+	float WaitingTimeBetweenPhrasesDiscontent = 30.0f;
+
+	FTimerHandle SpeechTimerHandle;
+	int32 CurrentSpeechIndex;
+
+	void Speak();
+
+
+};
