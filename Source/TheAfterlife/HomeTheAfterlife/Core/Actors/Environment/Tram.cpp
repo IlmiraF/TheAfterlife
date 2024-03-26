0d6e3b722c15736a52b549214eba2375ede21c1a
VlasovEgor <vlasovegor2@gmail.com>
2024-03-26 20:29:20 +0300

    fix movement conductor

--- Source/TheAfterlife/HomeTheAfterlife/Core/Actors/Environment/Tram.cpp
+++ Source/TheAfterlife/HomeTheAfterlife/Core/Actors/Environment/Tram.cpp
@@ -36,7 +36,7 @@ ATram::ATram()
 
 void ATram::TriggerToPlatfromOnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {	
-	Conductor->AttachToActor(this, FAttachmentTransformRules::SnapToTargetIncludingScale);
+	Conductor->AttachToComponent(TramMesh, FAttachmentTransformRules::KeepWorldTransform);
 
 	bIsMoving = true;
 
